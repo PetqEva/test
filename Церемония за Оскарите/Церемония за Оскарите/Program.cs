094096a2d2452fc3cb116f58f3cf5f4e9b0bbea9
PetqEva <petq_69@mail.bg>
2022-05-05 08:28:58 +0300

    dfgh

---
+++ Церемония за Оскарите/Церемония за Оскарите/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Oskari
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double naem = double.Parse(Console.ReadLine());
+            double statuetki = naem - naem * 0.30;
+            double ketaring = statuetki - statuetki * 0.15;
+            double ozvuchavane = ketaring * 0.50;
+            double sum = naem + statuetki + ketaring + ozvuchavane;
+            Console.WriteLine($"{sum:F2}");
+        }
+    }
+}
